6c04485aa2ac5bb999e3e962dae86c3bcf12098d
Benjamin Beilharz <b@ben.graphics>
2025-05-21 15:03:11 +0200

    ready for ws

--- numpyro_oop/monitoring.py
+++
@@ -1,9 +0,0 @@
-"""
-This modules adds monitoring support of the experiments via Weights and Biases.
-"""
-
-from core import BaseNumpyroModel
-import wandb
-
-def start_experiment(experiment_name: str, model: BaseNumpyroModel):
-    wandb.init(experiment_name)
